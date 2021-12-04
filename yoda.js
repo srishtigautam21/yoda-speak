fbb69f06a19066e0da573dc3957f7cc5b9f1ba76
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-04 16:57:45 +0530

    initial commit

---
+++ yoda.js
@@ -0,0 +1,33 @@
+var buttonTranslate=document.querySelector("#btn-translate");
+var inputText=document.querySelector("#txt-input");
+var outputText=document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/yoda.json"
+
+function geturl(input)
+{
+    return serverURL + "?" + "input=" + input
+}
+function errorHandler(error)
+{
+    console.log("Got error",error);
+    alert("There is something wrong with the server!! Try connecting after sometime")
+}
+
+function clickEventHandler()
+{
+    var text=inputText.value
+    fetch(geturl(text))
+    
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        outputText.innerText=translatedText;
+        })
+    
+    .catch(errorHandler)
+    
+};
+
+buttonTranslate.addEventListener("click",clickEventHandler)
+
+
